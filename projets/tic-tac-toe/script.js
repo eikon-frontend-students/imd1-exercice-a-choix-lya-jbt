11fc3147d6565fe16eec2ab6a43545a88d6d4330
lya.jbt <lya.joubert@studentfr.ch>
2025-05-26 11:57:59 +0200

    trying to changes the x and 0 but I'm failing

--- projets/tic-tac-toe/script.js
+++ projets/tic-tac-toe/script.js
@@ -1,8 +1,8 @@
 const board = document.getElementById("board");
 const statusText = document.getElementById("status");
 const resetBtn = document.getElementById("reset");
-const XImg = "icone/heart.jpg";
-const OImg = "icone/star.jpg";
+const X = "icone/heart.jpg";
+const O = "icone/star.jpg";
 
 let currentPlayer = "X";
 let grid = Array(9).fill(null);
